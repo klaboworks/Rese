20bc3991c5e3e3da51ebfcfe796653d7571ceb8b
KenTanaka <klaboworks@gmail.com>
2024-10-13 13:55:47 +0900

    飲食店一覧ビュー作成 #2

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ShopManagementController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ShopController::class, 'index']);
 
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/shop', [ShopManagementController::class, 'index'])->name('shop.index');

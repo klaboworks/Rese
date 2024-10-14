d9fd3450d93ec291b41958ae846042bd756f45fc
KenTanaka <klaboworks@gmail.com>
2024-10-14 21:05:27 +0900

    店舗詳細画面戻るボタン設置とレスポンシブ対応 #5

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -1,46 +1,49 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/detail.css') }}">
+<link rel="stylesheet" href="{{ asset('css/detail.css') }}">
 @endsection
 
 @section('content')
-    <section class="shop-detail">
-        <div class="shop-detail__inner">
-            <div class="detail-block__left">
-                <div class="menu">メニューボタン</div>
-                <div class="shop-info">
+<section class="shop-detail">
+    <div class="shop-detail__inner">
+        <div class="detail-block__left">
+            <div class="btn-menu">メニューボタン</div>
+            <div class="shop-info">
+                <div class="shop-info__heading">
+                    <a href="{{route('shop.index')}}" class="btn-back">&lt</a>
                     <h2 class="shop_name">{{ $shop->shop_name }}</h2>
-                    <div class="shop_image">
-                        <img src="{{ $shop->shop_image ? asset('storage/' . $shop->shop_image) : asset('storage/' . 'noimage.png') }}"
-                            alt="">
-                    </div>
-                    <div class="shop_tags">
-                        <span class="shop_area">#{{ $shop->area->area_name }}</span>
-                        <span class="shop_genre">#{{ $shop->genre->genre_name }}</span>
-                    </div>
-                    <p class="shop_description">{{ $shop->shop_description }}</p>
                 </div>
-            </div>
-            <div class="detail-block__right">
-                <div class="reservation-panel">
-                    <h3 class="form-title form-elements">予約</h3>
-                    <form action="" method="post" class="shop-reservation">
-                        <ul class="form-elements">
-                            <li class="form__date">
-                                <input type="date" name="date">
-                            </li>
-                            <li class="form__time full">
-                                <input type="time" name="time" step="1800" class="full">
-                            </li>
-                            <li class="form__number full">
-                                <input type="number" name="number" class="full">
-                            </li>
-                        </ul>
-                        <button class="form-btn">予約する</button>
-                    </form>
+                <div class="shop_image">
+                    <img src="{{ $shop->shop_image ? asset('storage/' . $shop->shop_image) : asset('storage/' . 'noimage.png') }}"
+                        alt="">
                 </div>
+                <div class="shop_tags">
+                    <span class="shop_area">#{{ $shop->area->area_name }}</span>
+                    <span class="shop_genre">#{{ $shop->genre->genre_name }}</span>
+                </div>
+                <p class="shop_description">{{ $shop->shop_description }}</p>
             </div>
         </div>
-    </section>
+        <div class="detail-block__right">
+            <div class="reservation-panel">
+                <h3 class="form-title form-elements">予約</h3>
+                <form action="" method="post" class="shop-reservation">
+                    <ul class="form-elements">
+                        <li class="form__date">
+                            <input type="date" name="date">
+                        </li>
+                        <li class="form__time full">
+                            <input type="time" name="time" step="1800" class="full">
+                        </li>
+                        <li class="form__number full">
+                            <input type="number" name="number" class="full">
+                        </li>
+                    </ul>
+                    <button class="form-btn">予約する</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ShopManagementController;
 
-Route::get('/', [ShopController::class, 'index']);
+Route::get('/', [ShopController::class, 'index'])->name('shop.index');
 Route::get('/detail/{shop}', [ShopController::class, 'detail'])->name('shop.detail');
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('/shop', [ShopManagementController::class, 'index'])->name('shop.index');
-    Route::get('/shop/create', [ShopManagementController::class, 'create'])->name('shop.create');
-    Route::post('/shop/store', [ShopManagementController::class, 'store'])->name('shop.store');
+    Route::get('/shop', [ShopManagementController::class, 'index'])->name('admin.shop.index');
+    Route::get('/shop/create', [ShopManagementController::class, 'create'])->name('admin.shop.create');
+    Route::post('/shop/store', [ShopManagementController::class, 'store'])->name('admin.shop.store');
 });

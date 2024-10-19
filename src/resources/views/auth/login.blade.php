78b4ef227660d33c9034e0f5f08e307301ebaf39
KenTanaka <klaboworks@gmail.com>
2024-10-19 13:03:39 +0900

    ログイン、会員登録ビュー修正 #3

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <section class="login">
         <div class="login__inner">
             <div class="login__heading">
-                <div>menu button</div>
+                <x-menu-box />
             </div>
             <div class="login__body">
                 <div class="login__panel">
@@ -18,8 +18,14 @@
                     <div class="panel__body">
                         <form action="/login" method="post" class="login__form">
                             @csrf
-                            <input type="text" name="email" placeholder="Email" class="input__email">
-                            <input type="password" name="password" placeholder="Password" class="input__pass">
+                            <div class="input__elements">
+                                <img src="{{ asset('icons/email.png') }}" alt="">
+                                <input type="text" name="email" placeholder="Email" class="input__email">
+                            </div>
+                            <div class="input__elements">
+                                <img src="{{ asset('icons/password.png') }}" alt="">
+                                <input type="password" name="password" placeholder="Password" class="input__pass">
+                            </div>
                             <button type="submit" class="login__btn">ログイン</button>
                         </form>
                     </div>
